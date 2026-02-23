e82759a4c423c24cdb9290d65c517eef13d80358
Lovely Bisht <bishtlovely28@gmail.com>
2026-02-23 23:41:06 +0530

    Removed sensitive files

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-if weather:
-            record = WeatherRecord(
-                city=weather["city"],  
-                temperature=weather["temperature"],
-                description=weather["description"],
-                humidity=weather["humidity"],
-                latitude=weather["latitude"],
-                longitude=weather["longitude"]
-            )
